e2109c095f193f04b882675b80346872d5927d3c
trapeznikova-evgenija <trapeznikiva.evg@yandex.ru>
2019-06-01 13:10:14 +0300

    генерация фигур

--- Assets/Scripts/ShapesGenerator.cs
+++ Assets/Scripts/ShapesGenerator.cs
@@ -1,18 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using System;
 using System.Linq;
 using System.Text;
+//координаты выбирать из заданных диапазонов, будет более струртурировано
+//размеры тоже выбрать примеирно 5 штук
 
+   //background белый 
 using Random = UnityEngine.Random;
 public class ShapesGenerator : MonoBehaviour
 {
     public GameObject[] objects;
     private GameObject inst_obj;
+    public Button[] but;
+    public Button button1;
+    public Button button2;
+    [SerializeField]
+    private int[] counter = new int[2];
+
+    public void IsCircle()
+    {
+       if(counter[1] > counter[0])
+       {
+           Debug.Log("Верно");
+       }
+       else
+       {
+           Debug.Log("Неверно");
+       }
+    }
+    public void IsRect()
+    {
+        if (counter[0] > counter[1])
+        {
+            Debug.Log("Верно");
+        }
+        else
+        {
+            Debug.Log("Неверно");
+        }
+    }
+    
+
+
     // Start is called before the first frame update
     void Start()
     {
+        for (int i = 0; i < counter.Length; i++)
+        {
+            counter[i] = 0;
+        }
         for (int j = 0; j < 5; ++j)
         {
             int randomX = Random.Range(1, 320);
@@ -21,26 +60,32 @@ public class ShapesGenerator : MonoBehaviour
             int randRotation = Random.Range(100, 150);
             inst_obj = Instantiate(objects[1], new Vector2(randomX, randomY), Quaternion.identity) as GameObject;
             inst_obj.transform.localScale = new Vector3(randScale, randScale, 1);
-            inst_obj.transform.localRotation = Quaternion.Euler(randRotation, randRotation, 1);
+            inst_obj.transform.rotation = Quaternion.Euler(0, 0, randRotation);
+            counter[0]++;
         }
 
-        for (int j = 0; j < 5; ++j)
+        for (int j = 0; j < 4; ++j)
         {
             int randomX = Random.Range(1, 320);
             int randomY = Random.Range(300, 720);
             int randScale = Random.Range(100, 150);
             inst_obj = Instantiate(objects[0], new Vector2(randomX, randomY), Quaternion.identity) as GameObject;
             inst_obj.transform.localScale = new Vector3(randScale, randScale, 1);
+            counter[1]++;
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
-            Debug.Log("Space key was pressed.");
+        for (int i = 0; i < but.Length; i++)
+        {
+            print(but[i]);
+        }
+    }
 
-        if (Input.GetKeyUp(KeyCode.Mouse0))
-            Debug.Log("Space key was released.");
+    void MixShapes()
+    {
+
     }
 }
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text text;
+    public string message = "Время вышло!";
+    // Start is called before the first frame update
+    public float timer = 0;
+    public int max = 10;
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        text.text = (max - timer).ToString("#.");
+
+        if (text.text == "0")
+        {
+            GUI.Label(new Rect(Screen.width / 2, Screen.height / 2, 200f, 200f), message);
+        }
+    }
+}
